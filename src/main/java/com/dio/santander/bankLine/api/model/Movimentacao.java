a3d923b586f2385a4ddf9a7747e1f57607b42275
Marcos Paulo Ignacio <94194033+Marcos-Ignacio@users.noreply.github.com>
2022-05-11 15:43:24 -0300

    Classe de Movimentação adicionada

--- src/main/java/com/dio/santander/bankLine/api/model/Movimentacao.java
+++ src/main/java/com/dio/santander/bankLine/api/model/Movimentacao.java
@@ -1,5 +1,51 @@
 package com.dio.santander.bankLine.api.model;
 
-public class Movimentacao {
+import java.time.LocalDateTime;
 
+public class Movimentacao {
+	
+	private Integer id;
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	private LocalDateTime dataHora;
+	public LocalDateTime getDataHora() {
+		return dataHora;
+	}
+
+	public void setDataHora(LocalDateTime dataHora) {
+		this.dataHora = dataHora;
+	}
+	
+	private String descricao;
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	private Double valor;
+	public Double getValor() {
+		return valor;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+	
+	private MovimentacaoTipo tipo;
+	public MovimentacaoTipo getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(MovimentacaoTipo tipo) {
+		this.tipo = tipo;
+	}
 }
